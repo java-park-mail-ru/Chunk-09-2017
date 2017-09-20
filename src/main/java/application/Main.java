9e16ade4f0081efe21dddc0805fae6eac4a10213
Dmitriy Trubnikov <trubnikovdv@mail.ru>
2017-09-21 01:41:26 +0300

    checkstyle

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/application/Views.java
@@ -0,0 +1,43 @@
+package application;
+
+public class Views {
+
+    static final int MIN_USERNAME_LENGTH = 4;
+    static final int MIN_PASSWORD_LENGTH = 6;
+
+    public static class Response {}
+
+    public static final class Profile extends Response {
+
+        private String username;
+        private String email;
+
+        Profile(String username, String email) {
+            this.username = username;
+            this.email = email;
+        }
+        Profile(User user) {
+            this.username = user.getUsername();
+            this.email = user.getEmail();
+        }
+
+        public String getUsername() {
+            return this.username;
+        }
+        public String getEmail() {
+            return email;
+        }
+    }
+
+    public static final class BadRequest extends Response {
+        private String errorMessage;
+
+        BadRequest(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+    }
+}
