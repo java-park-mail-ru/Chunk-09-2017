4c78fbf4a3b4da2825c93158d7aafa94373f9ae3
Dmitriy Trubnikov <trubnikovdv@mail.ru>
2017-10-19 04:36:26 +0300

    changed init.sql, heroku deploy

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -10,11 +10,6 @@ CREATE TABLE score (
 );
 
 
--- ALTER TABLE score OWNER TO trubnikov;
-
---
--- Name: score_id_seq; Type: SEQUENCE; Schema: public; Owner: trubnikov
---
 
 CREATE SEQUENCE score_id_seq
 START WITH 1
@@ -23,19 +18,9 @@ NO MINVALUE
 NO MAXVALUE
 CACHE 1;
 
-
--- ALTER TABLE score_id_seq OWNER TO trubnikov;
-
---
--- Name: score_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: trubnikov
---
-
 ALTER SEQUENCE score_id_seq OWNED BY score.id;
 
 
---
--- Name: users; Type: TABLE; Schema: public; Owner: trubnikov
---
 
 CREATE TABLE users (
     id bigint NOT NULL,
@@ -45,12 +30,6 @@ CREATE TABLE users (
 );
 
 
--- ALTER TABLE users OWNER TO trubnikov;
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: trubnikov
---
-
 CREATE SEQUENCE users_id_seq
 START WITH 1
 INCREMENT BY 1
@@ -59,68 +38,22 @@ NO MAXVALUE
 CACHE 1;
 
 
--- ALTER TABLE users_id_seq OWNER TO trubnikov;
-
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: trubnikov
---
-
 ALTER SEQUENCE users_id_seq OWNED BY users.id;
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: trubnikov
---
 
 ALTER TABLE ONLY score ALTER COLUMN id SET DEFAULT nextval('score_id_seq'::regclass);
 
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: trubnikov
---
-
 ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
-
-
-
-
-
---
 
 ALTER TABLE ONLY users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
-
---
--- Name: schema_version_s_idx; Type: INDEX; Schema: public; Owner: trubnikov
---
-
-
---
--- Name: users_email_uindex; Type: INDEX; Schema: public; Owner: trubnikov
---
-
 CREATE UNIQUE INDEX users_email_uindex ON users USING btree (email);
 
-
---
--- Name: users_username_uindex; Type: INDEX; Schema: public; Owner: trubnikov
---
-
 CREATE UNIQUE INDEX users_username_uindex ON users USING btree (username);
-
-
---
--- Name: score_users_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: trubnikov
---
 
 ALTER TABLE ONLY score
     ADD CONSTRAINT score_users_id_fk FOREIGN KEY (id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
-
---
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
 
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
 REVOKE ALL ON SCHEMA public FROM postgres;
